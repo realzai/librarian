1e3cf0cc0c29e003f14ebca54ff2b3108ecd15a3
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-02-09 12:31:00 +0630

    finish book crud

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,40 @@ class BooksController < ApplicationController
   layout 'dashboard'
 
   def index
-    @books = Book.where(user: current_user)
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
     @genres = Genre::all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @genres = Genre::all
+  end
+
+  def update
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre_id)
   end
 end
